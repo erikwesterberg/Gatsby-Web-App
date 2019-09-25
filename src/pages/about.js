e9440286050d58431d3f8198abff67c7d0ac8baa
erka <erik.westerberg@hotmail.se>
2019-09-25 17:49:07 +0200

    created to more page component, and created a link from index to a new url

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <h1>About me</h1>
+)
+
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContactPage = () => (
+
+  <h1>Contact</h1>,
+  <p>Call or mail pls</p>
+)
+
+export default ContactPage;
