5291525bd86e5cf65bd290d79f0afd1e05d5a6bd
erka <erik.westerberg@hotmail.se>
2019-09-28 13:35:16 +0200

    creates a template folder and a new blog component

---
+++ src/templates/blog.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+
+const Blog = () => {
+  return (
+    <Layout>
+      this is the blog 
+    </Layout>
+  )
+}
+
+export default Blog;
